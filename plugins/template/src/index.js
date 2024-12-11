8e154025e8168f3055e8d18555925aa26e288509
Sroryi <133896936+Sroryi@users.noreply.github.com>
2024-12-11 10:30:37 -0600

    Update index.js

--- plugins/template/src/index.js
+++ plugins/template/src/index.js
@@ -1,9 +1,37 @@
 import { logger } from "@vendetta";
 import Settings from "./Settings";
+import { registerCommand } from "@vendetta/commands";
+//import testcmdexc from "./cmd",
+
+function testcmdexc(msg) {
+  try {
+        let output = ["uhh test\n"];
+        const data = "test test hi";
+  };
+      logger.log(msg)
+      findByProps("sendBotMessage").sendBotMessage(
+        ctx.channel.id,
+        output.join("\n"),
+      );
+}
 
 export default {
+  testcmd = registerCommand({
+      name: "command test lol",
+      displayName: "hi woe",
+
+      description: "command test yes",
+      displayDescription: "description is cool",
+
+      options,
+      execute: testcmdexc,
+      applicationId: -1,
+      inputType: 1,
+      type: 1,
+    });
     onLoad: () => {
-        logger.log("Hello world!");
+        logger.log("test log");
+        testcmd();
     },
     onUnload: () => {
         logger.log("Goodbye, world.");
