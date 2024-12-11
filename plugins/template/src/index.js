15db4fb07d7996f6be0f3922faa78dac189f5d9f
Sroryi <133896936+Sroryi@users.noreply.github.com>
2024-12-11 10:18:19 -0600

    Update index.js

--- plugins/template/src/index.js
+++ plugins/template/src/index.js
@@ -1,36 +1,10 @@
-import testcmd from "./cmd";
-import { registerCommand } from "@vendetta/commands";
+import { logger } from "@vendetta";
 import Settings from "./Settings";
-
-let testcmd;
+import { registerCommand } from "@vendetta/commands";
+import testcmdexc from "./cmd",
 
 export default {
-  onLoad: () => {
-    const categories = ["device", "hardware", "software", "discord", "react"]; // TODO: move this into storage or somewhere more global
-    let options = [
-      {
-        name: "ephemeral",
-        displayName: "ephemeral",
-        description: "Keep sysinfo ephemeral (default: true)",
-        displayDescription: "Keep sysinfo ephemeral (default: true)",
-
-        type: 5,
-        required: false,
-      },
-      ...categories.map((cat) => {
-        return {
-          name: cat,
-          displayName: cat,
-
-          description: `Display the ${cat} section. Set default in settings.`,
-          displayDescription: `Display the ${cat} section. Set default in settings.`,
-
-          type: 5,
-          required: false,
-        };
-      }),
-    ];
-    syscmd = registerCommand({
+  testcmd = registerCommand({
       name: "command test lol",
       displayName: "hi woe",
 
@@ -38,14 +12,17 @@ export default {
       displayDescription: "description is cool",
 
       options,
-      execute: sysinfo,
+      execute: testcmdexc,
       applicationId: -1,
       inputType: 1,
       type: 1,
     });
-  },
-  onUnload: () => {
-    syscmd();
-  },
-  settings: Settings,
-};
+    onLoad: () => {
+        logger.log("test log");
+        testcmd();
+    },
+    onUnload: () => {
+        logger.log("Goodbye, world.");
+    },
+    settings: Settings,
+}
