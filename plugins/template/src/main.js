f8891f1ce64176f84f13a0a16e5192f308845b92
Sroryi <133896936+Sroryi@users.noreply.github.com>
2025-02-11 13:04:50 -0600

    Create main.js

---
+++ plugins/template/src/main.js
@@ -0,0 +1,21 @@
+import { registerCommand } from "@vendetta/commands"
+import { findByProps } from "@vendetta/metro"
+
+const MessageActions = findByProps("sendMessage", "receiveMessage")
+
+let commands = [
+]
+
+commands.push(registerCommand({
+    name: "test !!",
+    displayName: "test yeah",
+    description: "it is a test or something",
+    displayDescription: "description or something i think",
+    applicationId: -1,
+    inputType: 1,
+    type: 1,
+
+    execute: (ctx) => MessageActions.sendMessage(ctx.channel.id, {
+        content: "test test haiii!! ^w^"
+    })
+}))
