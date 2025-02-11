67d8668fcd7e1224efbccd8706f5d5358f5b70dc
Sroryi <133896936+Sroryi@users.noreply.github.com>
2025-02-11 12:53:55 -0600

    Update index.ts

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -1,12 +1,21 @@
-import { logger } from "@vendetta";
-import Settings from "./Settings";
+import { registerCommand } from "@vendetta/commands"
+import { findByProps } from "@vendetta/metro"
 
-export default {
-    onLoad: () => {
-        logger.log("Hello world!");
-    },
-    onUnload: () => {
-        logger.log("Goodbye, world.");
-    },
-    settings: Settings,
-}
+const MessageActions = findByProps("sendMessage", "receiveMessage")
+
+let commands = [
+]
+
+commands.push(registerCommand({
+    name: "test !!",
+    displayName: "test yeah",
+    description: "it is a test or something",
+    displayDescription: "description or something i think",
+    applicationId: -1,
+    inputType: 1,
+    type: 1,
+
+    execute: (ctx) => MessageActions.sendMessage(ctx.channel.id, {
+        content: "test test haiii!! ^w^"
+    })
+}))
