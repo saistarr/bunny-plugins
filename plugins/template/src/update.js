08fde286264cb1f07f74db1c9d023837e3db5827
Sroryi <133896936+Sroryi@users.noreply.github.com>
2025-02-11 12:25:12 -0600

    Create update.js

---
+++ plugins/template/src/update.js
@@ -0,0 +1,55 @@
+try {
+    import { createList } from "../../lib/utility";
+} catch (ignore) {
+    
+}
+
+function ma(...a) { return [...a] }
+
+const update = [
+	createList(
+		"1.0.0",
+		ma("Created the Plugin (woah)"),
+	),
+	createList(
+		"1.0.1 - 1.0.3",
+		ma(
+			"[1.0.1] hmm1",
+			"[1.0.1] hmm2",
+			"[1.0.1] hmm3",			
+			"[1.0.22] hmm4",
+			"[1.0.22] hmm5",
+			"[1.0.22] hmm6"
+		),
+		ma(
+			"[1.0.2] hmm1",
+			"[1.0.24] hmm2",
+			"[1.0.3] hmm3"
+		),
+		ma(
+			"[1.0.21] hmm1",
+			"[1.0.22] hmm2",
+			"[1.0.23] hmm3",
+			"[1.0.3] hmm4",
+			"[1.0.3] hmm5",
+		)
+	),
+	createList(
+		"1.1",
+		ma(
+			"[1.1.0] hmm1",
+			"[1.1.0] hmm2",
+		),
+		ma(
+			"[1.1.0] hmm1",
+		)
+	),
+	createList(
+		"1.2",
+		null,
+		ma("[1.2] hmm1")
+	)
+]
+
+
+export default update.reverse();
