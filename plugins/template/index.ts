e7f5c7032838c075d2c57c56ce656467c7e74084
Sroryi <133896936+Sroryi@users.noreply.github.com>
2025-02-11 12:58:33 -0600

    Create index.ts

---
+++ plugins/template/index.ts
@@ -0,0 +1,14 @@
+{
+    "name": "Command Test",
+    "description": "command thingy i think",
+    "authors": [
+        {
+            "name": "",
+            "id": ""
+        }
+    ],
+    "main": "src/main.js",
+    "vendetta": {
+        "icon": "ic_application_command_24px"
+    }
+}
