56a2b92da94cc2220563069b80341d9353785bf3
Sroryi <133896936+Sroryi@users.noreply.github.com>
2025-02-13 16:10:32 -0600

    Update main.js

--- plugins/template/src/main.js
+++ plugins/template/src/main.js
@@ -3,17 +3,22 @@ import { registerCommand } from "@vendetta/commands"
 let testCommand = [
 ]
 
+const ranItem = async function () {
+	var items = ["hiii!!","boo..","woe....","<3"];
+    return items[Math.floor(Math.random()*items.length)]
+}
+
 export const onLoad = () => {
     testCommand = registerCommand({
-        name: "test !!",
-        displayName: "test 2 !!",
+        name: "random stringgg...",
+        displayName: "random string!",
         description: "hi !!",
         displayDescription: "hello !!",
         applicationId: '-1',
         inputType: 1,
         type: 1,
 
-        execute: async () => { return {content:"e17dbc8227aeef69e0144649714738ea","text":"hmmmmm... hai!!! ^w^"}}
+        execute: async () => { return {content:ranItem()}}
     });
 }
 
