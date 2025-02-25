1a173648d701337b288033de24c350d131191dad
saistarr <saistarr@users.noreply.github.com>
2025-02-25 18:21:24 +0000

    deploy: 760cdee6ece9c91450ae667e414064aeec0e5052

--- HideThemeBackground/index.js
+++ HideThemeBackground/index.js
@@ -1,1 +1,1 @@
-(function(e){"use strict";e.colorsPref.customBackground=type!=="shown"?type:null})(vendetta.lib.addons.themes.colors.preferences.ts);
+(function(e){"use strict";e.colorsPref.customBackground=type!=="shown"?type:null})(vendetta.addons.themes.colors.preferences.ts);
