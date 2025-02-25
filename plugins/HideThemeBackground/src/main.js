fd0d404b5a52e110f22ece063a7c85d401134294
Sai <133896936+saistarr@users.noreply.github.com>
2025-02-25 13:45:42 -0600

    Update main.js

--- plugins/HideThemeBackground/src/main.js
+++ plugins/HideThemeBackground/src/main.js
@@ -1,3 +1,3 @@
-import { colorsPref } from "@lib/addons/themes/colors/preferences"
+import { colorsPref } from "@themes"
 colorsPref.customBackground = "shown"
 // sorry, i'm just learning and i've never used javascript before lol
