760cdee6ece9c91450ae667e414064aeec0e5052
Sai <133896936+saistarr@users.noreply.github.com>
2025-02-25 12:21:05 -0600

    Update main.js

--- plugins/HideThemeBackground/src/main.js
+++ plugins/HideThemeBackground/src/main.js
@@ -1,3 +1,3 @@
-import { colorsPref } from "@vendetta/lib/addons/themes/colors/preferences.ts"
+import { colorsPref } from "@vendetta/addons/themes/colors/preferences.ts"
 colorsPref.customBackground = type !== "shown" ? type : null;
 // sorry, i'm just learning and i've never used javascript before lol
