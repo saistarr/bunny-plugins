627aebeef4ca11b1e8478d89982bbab2d376b7d9
Sai <133896936+saistarr@users.noreply.github.com>
2025-02-25 13:00:55 -0600

    Update main.js

--- plugins/HideThemeBackground/src/main.js
+++ plugins/HideThemeBackground/src/main.js
@@ -1,3 +1,4 @@
-import { colorsPref } from "@vendetta/colors"
+import { storage } from "@vendetta/plugin"
+const colorsPref = storage.BunnyColorPreferencesStorage
 colorsPref.customBackground = "shown"
 // sorry, i'm just learning and i've never used javascript before lol
