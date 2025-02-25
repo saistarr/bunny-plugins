e6dfcf83ac8fcf595b0dd12b6b0744fa06192d5f
Sai <133896936+saistarr@users.noreply.github.com>
2025-02-25 13:16:31 -0600

    Update main.js

--- plugins/HideThemeBackground/src/main.js
+++ plugins/HideThemeBackground/src/main.js
@@ -1,3 +1,3 @@
-import { colorsPref } from "@themes/colors/preferences"
-colorsPref.customBackground = "shown"
+import { BunnyColorPreferencesStorage } from "@themes/colors/preferences"
+BunnyColorPreferencesStorage.customBackground = "shown"
 // sorry, i'm just learning and i've never used javascript before lol
